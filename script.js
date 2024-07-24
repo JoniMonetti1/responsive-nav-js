50b145631ffacff64c71f5f1e092ee914aeb9811
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2024-07-24 01:35:47 -0300

    Proyecto finalizado

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector('.movil-menu');
+    const navBar = document.querySelector('.nav-bar');
+    const socialMedia = document.querySelector('.social-media');
+    
     hamburger.addEventListener("click", () => {
-        const navBar = document.querySelector('.nav-bar');
         navBar.classList.toggle('active');
-
-        const socialMedia = document.querySelector('.social-media');
         socialMedia.classList.toggle('active');
     })
 
-    toggleMenu.addEventListener('click', () => {
-        navContent.classList.toggle('active');
+    //se seleccionan todos los elementos a del ul
+    const navLinks = document.querySelectorAll('.nav-bar ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) { //Es importante usar function porque sino this no funciona de la misma manera que con una arrow function
+            //evita el comportamiento default del elemento
+            event.preventDefault(); 
+
+            // Quita la clase active del enlace que la tenga
+            document.querySelector('.nav-bar ul li a.active').classList.remove('active');
+
+            // Añade la clase active al enlace clicado
+            this.classList.add('active');
+        })
     })
 });
     
